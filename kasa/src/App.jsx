86680c748112fc7cda8e262758346382c261d98d
Jérémy Catoire <j.catoire60@gmail.com>
2025-07-31 17:45:22 +0200

    premier commit projet7

---
+++ kasa/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './component/navbar';
+import './App.css';
+import Banner from './component/banner';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
---
+++ kasa/src/component/banner.jsx
@@ -0,0 +1,13 @@
+    import React from 'react'
+    import './banner.css';
+
+    function Banner() {
+    return (
+        <div className='banner'>
+            <img src="/banner.png" alt="banniere" />
+            <h1 className="banner-text">Chez vous, partout et ailleurs</h1>
+        </div>
+    )
+    }
+
+    export default Banner
