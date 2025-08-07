f45293ca79970e25d5a499b9901dd31ebde75484
Jérémy Catoire <j.catoire60@gmail.com>
2025-08-07 17:25:21 +0200

    bien avance haut de page

--- kasa/src/component/navbar.jsx
+++ kasa/src/component/navbar.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
 import './navbar.css';
-
 
 function Navbar() {
   return (
-      <nav className="navbar">
-        <div className="navbar__logo">
-            <img src="logo.png" alt='logo'/>
-    </div>
-    <div>Accueil</div>
-    <div>A propos</div>
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <img src="/logo.png" alt="Kasa" />
+      </div>
+      <div className="navbar__links">
+        <a href="#">Accueil</a>
+        <a href="#">À propos</a>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
