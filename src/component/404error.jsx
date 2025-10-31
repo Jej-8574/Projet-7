33b39e149310aca4ef6b2d36dd935ed815a07693
Jérémy Catoire <j.catoire60@gmail.com>
2025-10-31 10:49:02 +0100

    fix github pages

--- src/component/404error.jsx
+++ src/component/404error.jsx
@@ -10,7 +10,7 @@ function Error404() {
       <div className="container">
         <h1 className="error-code">404</h1>
         <p className="error-message">
-          Oups! La page que vous demandez n'existe pas.
+        La page que vous demandez n'existe pas.
         </p>
         <Link to="/" className="error-link">
           Retourner sur la page d'accueil
